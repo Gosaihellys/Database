9bd87d0cb0822949f715b948ef5d2de8b0c3606c
Gosaihellys <97091992+Gosaihellys@users.noreply.github.com>
2023-12-22 21:57:30 +0530

    first commit

---
+++ Module 3.1/simple interest.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	float amountP, timeN, rateR, SI;
+	printf("enter amount:");
+	scanf("%f",&amountP);
+	
+	printf("enter time:");
+	scanf("%f",&timeN);
+	
+	printf("enter rate of interest:");
+	scanf("%f",&rateR);
+	
+	SI=(amountP*timeN*rateR)/100;
+	printf("simple interest %f",SI);
+	
+	return 0;
+	
+}
